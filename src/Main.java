6cc188980e37b00df65ee347b6ba5060a2d98cb3
f-feijoo <84591753+f-feijoo@users.noreply.github.com>
2023-04-26 16:33:16 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.setEdad(23);
+        cliente.setNombre("Francisco");
+        cliente.setTelefono(123456789);
+        cliente.setCredito(150000);
+        System.out.println(cliente.getEdad());
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getTelefono());
+        System.out.println(cliente.getCredito());
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.setEdad(29);
+        trabajador.setNombre("Juano");
+        trabajador.setTelefono(166456789);
+        trabajador.setSalario(75000);
+        System.out.println(trabajador.getEdad());
+        System.out.println(trabajador.getNombre());
+        System.out.println(trabajador.getTelefono());
+        System.out.println(trabajador.getSalario());
+    }
+
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getTelefono() {
+        return this.telefono;
+    }
+}
+
+class Cliente extends Persona {
+    private int credito;
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
+    public int getCredito() {
+        return this.credito;
+    }
+}
+
+class Trabajador extends Persona {
+    private int salario;
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public int getSalario() {
+        return this.salario;
+    }
+}
